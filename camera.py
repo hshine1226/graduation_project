0d43b2928e19669d9af9a6f8335a2d7766c93a7d
hshine1226 <31975706+hshine1226@users.noreply.github.com>
2018-09-10 23:28:16 +0900

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,31 +1,26 @@
 import RPi.GPIO as GPIO
 from picamera import PiCamera
-import time
 from time import sleep
 import datetime
 
 camera = PiCamera()
 
 GPIO.setmode(GPIO.BCM)
-PIR_PIN1 = 7 #humansensor1
-PIR_PIN2 = 8 #humansensor2
+PIR_PIN1 = 3 #FC-51
  
 GPIO.setup(PIR_PIN1, GPIO.IN)
-GPIO.setup(PIR_PIN2, GPIO.IN)
  
 try:     
     while True:
-        if GPIO.input(PIR_PIN1):
-            print "Motion Sensor1 Detect"            
-            
-            if GPIO.input(PIR_PIN1):
-                camera.start_preview()
-                now = datetime.datetime.now()
-                nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
-                camera.capture('/home/pi/'+str(nowDatetime)+'.jpg')
-                camera.stop_preview()
-        time.sleep(2)
+      if (!GPIO.input(PIR_PIN1)):    
+        print "FC-51 Detect"
+        camera.start_preview()
+        sleep(2)
+        now = time.localtime()
+        camera.capture('/home/pi/'+now+'image.jpg')
+        camera.stop_preview()
+        time.sleep(5)
  
 except KeyboardInterrupt:
-    print "Quit"
+    print “ Quit”
     GPIO.cleanup()
