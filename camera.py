f9860871c8d430bf91b9fda92cd58beb0b4b79ce
hshine1226 <31975706+hshine1226@users.noreply.github.com>
2018-09-10 22:26:15 +0900

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,11 +1,29 @@
+import RPi.GPIO as GPIO
 from picamera import PiCamera
 from time import sleep
 import datetime
 
 camera = PiCamera()
 
-camera.start_preview()
-sleep(2)
-now = time.localtime()
-camera.capture('/home/pi/'+now+'image.jpg')
-camera.stop_preview()
+GPIO.setmode(GPIO.BCM)
+PIR_PIN1 = 7 #인체감지센서1
+PIR_PIN2 = 8 #인체감지센서2
+ 
+GPIO.setup(PIR_PIN, GPIO.IN)
+ 
+try:     
+    while True:
+        if GPIO.input(PIR_PIN1):
+            print "Motion Sensor1 Detect"
+            if GPIO.input(PIR_PIN2):
+              print "Motion Sensor2 Detect"
+              camera.start_preview()
+              sleep(2)
+              now = time.localtime()
+              camera.capture('/home/pi/'+now+'image.jpg')
+              camera.stop_preview()
+        time.sleep(0.05)
+ 
+except KeyboardInterrupt:
+    print “ Quit”
+    GPIO.cleanup()
