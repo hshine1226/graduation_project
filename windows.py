f63b7b0756501dda5a27f41abcacb3a3481f6753
hshine1226 <31975706+hshine1226@users.noreply.github.com>
2018-09-10 23:26:15 +0900

    Update windows.py

--- windows.py
+++ windows.py
@@ -1,22 +1,28 @@
 import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM) 
+from picamera import PiCamera
+from time import sleep
+import datetime
+import os
 
-sensor = 23
-led = 22
+camera = PiCamera()
 
-GPIO.setup(sensor, GPIO.IN)
-GPIO.setup(led, GPIO.OUT)
-
-print "Waitig for sensor to settle"
-time.sleep(2)
-print "Detecting motion"
-
-while True:
-	if GPIO.input(sensor):
-		print "Motion Detected"
-		GPIO.output(led, True)
-		time.sleep(2)
-	GPIO.output(led, False)
-	time.sleep(2)
-[출처] 라즈베리파이 - 인체감지센서 HC-SR501 사용하기|작성자 코스모스
+GPIO.setmode(GPIO.BCM)
+PIR_PIN1 = 7 #Motion Detect Sensor1
+PIR_PIN2 = 8 #Motion Detect Sensor2
+ 
+GPIO.setup(PIR_PIN1, GPIO.IN)
+GPIO.setup(PIR_PIN2, GPIO.IN)
+ 
+try:     
+    while True:
+        if GPIO.input(PIR_PIN1):
+            print "Motion Sensor1 Detect"
+            if GPIO.input(PIR_PIN2):
+            	print "Motion Sensor2 Detect"      
+		print "창문 침입이 감지되었습니다."
+		#os.system('gammu sendsms TEXT 01073205117 -unicode -text "[SOS]\n도어락이 파손되었습니다.\n주소 : 한국기술교육대학교\n2공학관 119호"')
+        time.sleep(0.05)
+ 
+except KeyboardInterrupt:
+    print “ Quit”
+    GPIO.cleanup()
