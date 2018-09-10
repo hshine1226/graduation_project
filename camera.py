64e175f249964384dd18e286de661f1b2d4907f5
hshine1226 <hshine1226@gmail.com>
2018-09-10 23:25:28 +0900

    180910

--- camera.py
+++ camera.py
@@ -1,29 +1,31 @@
 import RPi.GPIO as GPIO
 from picamera import PiCamera
+import time
 from time import sleep
 import datetime
 
 camera = PiCamera()
 
 GPIO.setmode(GPIO.BCM)
-PIR_PIN1 = 7 #인체감지센서1
-PIR_PIN2 = 8 #인체감지센서2
+PIR_PIN1 = 7 #humansensor1
+PIR_PIN2 = 8 #humansensor2
  
-GPIO.setup(PIR_PIN, GPIO.IN)
+GPIO.setup(PIR_PIN1, GPIO.IN)
+GPIO.setup(PIR_PIN2, GPIO.IN)
  
 try:     
     while True:
         if GPIO.input(PIR_PIN1):
-            print "Motion Sensor1 Detect"
-            if GPIO.input(PIR_PIN2):
-              print "Motion Sensor2 Detect"
-              camera.start_preview()
-              sleep(2)
-              now = time.localtime()
-              camera.capture('/home/pi/'+now+'image.jpg')
-              camera.stop_preview()
-        time.sleep(0.05)
+            print "Motion Sensor1 Detect"            
+            
+            if GPIO.input(PIR_PIN1):
+                camera.start_preview()
+                now = datetime.datetime.now()
+                nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+                camera.capture('/home/pi/'+str(nowDatetime)+'.jpg')
+                camera.stop_preview()
+        time.sleep(2)
  
 except KeyboardInterrupt:
-    print “ Quit”
+    print "Quit"
     GPIO.cleanup()
