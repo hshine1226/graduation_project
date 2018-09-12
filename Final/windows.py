30a687aab1c8c73a9af348eca4066bc77fc4924b
hshine1226 <31975706+hshine1226@users.noreply.github.com>
2018-09-12 17:53:23 +0900

    Update windows.py

--- Final/windows.py
+++ Final/windows.py
@@ -13,19 +13,23 @@ PIR_PIN2 = 8 #Motion Detect Sensor2
  
 GPIO.setup(PIR_PIN1, GPIO.IN)
 GPIO.setup(PIR_PIN2, GPIO.IN)
- 
-try:     
-    while True:
-        if GPIO.input(PIR_PIN1):
-            print "Motion Sensor1 Detect"
-            sleep(0.5)
-            if GPIO.input(PIR_PIN2):
-                print "Motion Sensor2 Detect"      
-		print "Help~~"
-		sleep(0.1)
+
+cnt = 0
+try:
+	while True:
+		if GPIO.input(PIR_PIN1):
+			print "Motion Sensor1 Detect"			
+			cnt = 1
+			sleep(0.5)
+		if cnt == 1:
+			if GPIO.input(PIR_PIN2):
+				print "Motion Sensor2 Detect"
+				print "Help~~"
+				cnt = 0
+				sleep(0.5)
 		
 		#os.system('gammu sendsms TEXT 01073205117 -unicode -text "[SOS]\n도어락이 파손되었습니다.\n주소 : 한국기술교육대학교\n2공학관 119호"')
  
 except KeyboardInterrupt:
-    print "Quit"
-    GPIO.cleanup()
+	print "Quit"
+	GPIO.cleanup()
